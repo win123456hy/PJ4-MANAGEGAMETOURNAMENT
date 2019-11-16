a5c25682aff544293c593c403b3f496375c7b9de
Đạt Dev <datamity@gmail.com>
2019-11-16 14:00:57 +0700

    init project backend

---
+++ Backend/tournament/src/main/java/com/game/tournament/TournamentApplication.java
@@ -0,0 +1,21 @@
+package com.game.tournament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = { //
+        DataSourceAutoConfiguration.class, //
+        DataSourceTransactionManagerAutoConfiguration.class, //
+        HibernateJpaAutoConfiguration.class })
+public class TournamentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TournamentApplication.class, args);
+	}
+
+}
